9308a670f77380968ba7177439f1f5b8253e0565
Zhassulan Serikuly <74059375+zhassulaan@users.noreply.github.com>
2022-12-24 22:03:04 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h1>Hello React!!!</h1>
+    </div>
+  );
+}
+
+export default App;
